c1d5d56b7e967690a77814233bb87b9f6332d2f5
shadinmhd <shadinmhd97@gmail.com>
2024-06-24 11:49:38 +0530

    update: added content to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,34 @@
+import { useNavigate } from "react-router-dom"
+import Navbar from "../components/Navbar"
+import { useAuth } from "../context/auth"
+
 const Home = () => {
+
+	const { user } = useAuth()
+	const navigate = useNavigate()
+
+	const getStarted = () => {
+		if (user) {
+			navigate("/dashboard")
+		} else {
+			navigate("/login")
+		}
+	}
+
 	return (
-		<div className="bg-black">Home</div>
+		<div className="flex flex-col items-center h-screen text-white bg-black">
+			<Navbar />
+			<div className="flex flex-col items-center mt-32 h-full w-full">
+				<p className="text-5xl font-bold">Shorter</p>
+				<p className="text-lg font-semibold mt-5">
+					Turn lengthy web addresses into compact, manageable links.<br />
+					Simplify your URLs for easier sharing and cleaner appearance.
+				</p>
+				<button onClick={getStarted} className="bg-white text-black text-xl mt-5 font-bold px-3 py-1 rounded-lg">
+					Get Started
+				</button>
+			</div>
+		</div>
 	)
 }
 
